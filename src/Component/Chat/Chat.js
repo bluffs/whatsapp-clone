cbb8d2ae2c00240d612fd6eb089931ba83249899
Jalel Yakdi <jalelyakdi@MacBook-Pro.local>
2020-10-27 16:09:35 +0100

    :tada: setup the project

---
+++ src/Component/Chat/Chat.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './Chat.css'
+
+const Chat = () => {
+    return (
+        <div className="chat">
+            <h1>I'm the Chat</h1>
+        </div>
+    )
+}
+
+export default Chat
